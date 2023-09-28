7792fac7d8a3c1de38a9a50d9760312d9142c90f
Dell <Dell@192.168.0.184>
2023-09-28 10:18:43 +0530

    commit

---
+++ src/com/actitime/testScript/Client.java
@@ -0,0 +1,12 @@
+package com.actitime.testScript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Client{
+	@Test
+	public void createClient() {
+		Reporter.log("createclient",true);
+	}
+
+}
